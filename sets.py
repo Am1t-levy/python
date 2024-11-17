df00c3f30c1a4523b32812ae6673144111fd00f0
Am1t <amit122464@gmail.com>
2024-11-17 03:51:36 +0200

    Update sets.py

--- sets.py
+++ sets.py
@@ -1,4 +1,4 @@
-1.
+#1.
 my_sets = {1,2,3,4,5,}
 my_sets.add(6)
 print(my_sets)
@@ -6,7 +6,7 @@ my_sets.add(3)
 my_sets.remove(2)
 print(my_sets)
 
-2.
+#2.
  set_1 = {1,2,3,4}
  set_2 = {3,4,5,6} 
 set3 = set_1.union(set_2)
@@ -21,12 +21,12 @@ print("difference (set_1 - set_2):", difference_result)
 symmetric_difference_result = set_1.symmetric_difference(set_2)
 print("symmetric difference:", symmetric_difference_result)
 
-3.
+#3.
 numbers = [1,2,2,3,4,4,5]
 unique_members = list(set(numbers))
 print("unique numbers:", unique_members)
 
-4.
+#4.
 set_1 = {1,2,3,4}
 set_2 = {3,4,5,6} 
 
@@ -40,7 +40,7 @@ if 6 in set_2:
 else: 
     print("the number 6 is not in set_1")     
 
-5. 
+#5. 
 set_a = {1,2,3,4,5}
 set_a.add(6)
 print("after adding 6:", set_a)
